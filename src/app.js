d8747c09ad42670a2e0731f33640e198ef81b9af
Abir <abboura84@yahoo.fr>
2021-10-19 01:17:17 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,10 +13,17 @@ class SpencerAndWilliamsSearch {
   _registerEvents() {
     const autocomplete = document.querySelector('.autocomplete');
     const searchbox = document.querySelector('#searchbox input');
+	const suggestion = document.querySelector('.suggestions');
 
     searchbox.addEventListener('click', () => {
       autocomplete.style.display = 'block';
     });
+	
+	//TODO: retrieve suggestion value onlick and replace search text
+	
+   /* suggestions.addEventListener('click', () => {
+      document.getElementById("searchbox").value = document.getElementById("suggestions").value; 
+    });	*/
 
     searchbox.addEventListener('blur', () => {
       autocomplete.style.display = 'none';
